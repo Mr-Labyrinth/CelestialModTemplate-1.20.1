365dcb976b0a96c4b7a6d448f0d1a5b091332a56
qizhizaizhe <qizhishijie@163.com>
2025-04-17 21:00:31 +0800

    Initial commit

---
+++ src/main/java/com/lingyun/celestial/CelestialClient.java
@@ -0,0 +1,10 @@
+package com.lingyun.celestial;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CelestialClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
